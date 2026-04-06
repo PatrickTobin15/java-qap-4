28890c631dbb04db59b5f8c4d3f87ab07d488ae2
PatrickTobin15 <patrick.tobin@keyin.com>
2026-04-06 09:59:28 -0230

    Update Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -1,1 +1,25 @@
+public class Patient {
+    private int patientId;
+    private String patientFirstName;
+    private String patientLastName;
+    private String patientDOB;
 
+    public Patient(int patientId, String patientFirstName, String patientLastName, String patientDOB) {
+        this.patientId = patientId;
+        this.patientFirstName = patientFirstName;
+        this.patientLastName = patientLastName;
+        this.patientDOB = patientDOB;
+    }
+
+    // Getters
+    public int getPatientId() { return patientId; }
+    public String getPatientFirstName() { return patientFirstName; }
+    public String getPatientLastName() { return patientLastName; }
+    public String getPatientDOB() { return patientDOB; }
+
+    @Override
+    public String toString() {
+        return String.format("Patient [ID: %d | Name: %s %s | DOB: %s]",
+                patientId, patientFirstName, patientLastName, patientDOB);
+    }
+}
